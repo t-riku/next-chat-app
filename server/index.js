078934e607487622bfc51661e2b716b7d82e5cf2
t-riku <dingariku@icloud.com>
2022-11-05 16:24:46 +0900

    connect io

--- server/index.js
+++ server/index.js
@@ -4,16 +4,29 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  },
+});
 
 const PORT = 5000;
 
-// クライアントと通信
-io.on("connection", (socket) => {
-  console.log("クライアントと接続しました！");
+//test
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
 
-  socket.on("disconnect", () => {
-    console.log("クライアントと接続が切れました！");
+//クライアントと通信
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  //クライアントから受信
+  socket.on("send_message", (data) => {
+    console.log(data);
+    //クライアントへ返信
+    io.emit("received_message", data);
   });
 });
 
